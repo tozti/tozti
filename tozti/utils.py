4d712989aff4c49ab46b38160200c1bc8b214543
Lapinot <peio.borthelle@ens-lyon.fr>
2017-11-25 21:05:41 +0100

    Document helpers for routing.

--- tozti/utils.py
+++ tozti/utils.py
@@ -1,4 +1,10 @@
 class ResourceDef:
+    """Definition of a resource.
+
+    The method :meth:`get`, :meth:`post`, :meth:`put`, etc can be used as
+    decorators to specify the handler for the given HTTP method.
+    """
+
     def __init__(self, path, name=None):
         self._path = path
         self._name = name
@@ -6,54 +12,104 @@ class ResourceDef:
         self._prefix = ''
 
     def register(self, app):
+        """Add all our routes to the given `aiohttp.web.Application`."""
+
         resource = app.add_resource(self._path, name=self._name)
         resource.add_prefix(self._prefix)
         for m, h in self._routes.items():
             resource.add_route(m, h)
 
+    def route(self, *meth):
+        """Decorator (with arguments) used to specify HTTP handler."""
+
+        def decorator(handler):
+            for m in meth:
+                self._routes[m] = handler
+            return handler
+        return decorator
+
     def get(self, handler):
+        """Decorator used to specify ``GET`` method handler."""
+
         self._routes['GET'] = handler
         return handler
 
     def post(self, handler):
+        """Decorator used to specify ``GET`` method handler."""
+
         self._routes['POST'] = handler
         return handler
 
     def put(self, handler):
+        """Decorator used to specify ``PUT`` method handler."""
+
         self._routes['PUT'] = handler
         return handler
 
     def patch(self, handler):
+        """Decorator used to specify ``PATCH`` method handler."""
+
         self._routes['PATCH'] = handler
         return handler
 
     def delete(self, handler):
+        """Decorator used to specify ``DELETE`` method handler."""
+
         self._routes['DELETE'] = handler
         return handler
 
     def head(self, handler):
+        """Decorator used to specify ``HEAD`` method handler."""
+
         self._routes['HEAD'] = handler
         return handler
 
     def options(self, handler):
+        """Decorator used to specify ``OPTIONS`` method handler."""
+
         self._routes['OPTIONS'] = handler
         return handler
 
     def any(self, handler):
+        """Decorator used to specify handler for every method."""
+
         self._routes['*'] = handler
         return handler
 
 
-class Router:
+class RouterDef:
+    """Handle route definitions.
+
+    This object can be used as argument to
+    :meth:`aiohttp.web.UrlDispatcher.add_routes`.
+
+    Sample usage::
+
+        router = RouterDef()
+        resource = router.add_resource('/foo')
+
+        @resource.get
+        def handle_get(req):
+            return ...
+
+    See `aiohttp`_ for more informations on resources and routing.
+
+    .. _aiohttp: https://aiohttp.readthedocs.io/en/stable/web.html#resources-and-routes
+    """
+
     def __init__(self):
         self._resources = []
 
     def add_resource(self, path, name=None):
+        """Add and return a resource with given path to the router."""
+
         r = ResourceDef(path, name=name)
         self._resources.append(r)
         return r
 
     def add_prefix(self, prefix):
+        """Prefix every contained resource."""
+
         for r in self._resources:
             r._prefix = prefix
 
