a21f1a6991943bce5da4418b36f3234e40634255
flupe <lucas@escot.me>
2018-01-13 03:08:24 +0100

    added skeleton of dashboard & widget system + taxonomy

---
+++ client/components/Taxonomy.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="uk-container uk-padding">
+    <h1 class="uk-heading-primary">{{ $route.params.workspace }}</h1>
+    <p>taxonomy: <code>{{ $route.params.taxonomy }}</code></p>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
--- client/index.js
+++ client/index.js
@@ -1,18 +1,19 @@
-import App from './components/App.vue'
+import App       from './components/App.vue'
 import Dashboard from './components/Dashboard.vue'
+import Taxonomy  from './components/Taxonomy.vue'
 
 import store from './store'
 
 window.tozti = new Vue({
   el: '#app',
+  store,
 
   router: new VueRouter({
     routes: [
-      { path: '/', component: Dashboard }
+      { path: '/', component: Dashboard },
+      { path: '/:workspace/:taxonomy*', component: Taxonomy }
     ]
   }),
 
-  store,
   render: h => h(App)
 })
-
