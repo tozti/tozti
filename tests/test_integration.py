626fb8015a9272272a0d52f368f65140ac743d42
poechsel <pierre.oechsel@gmail.com>
2018-01-21 20:38:33 +0100

    improved tozti fixtures so that extensions can be installed

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -8,11 +8,19 @@ def tozti(request):
     """
     Fixture that:
         - clean the extension folder
+        - if extensions are to be installed, install the extensions
         - launch a tozti server
         - waits one second for it to be started
         - shutdown it at the end
+    To install extensions, add a mark named "extensions" to the test 
+    with arguments the extensions you want to install:
+        `@pytest.mark.extensions("ext1", "ext2")`
     """
     commons.empty_extensions_list()
+    extensions_marker = request.node.get_marker("extensions")
+    if extensions_marker is not None:
+        print(extensions_marker.args)
+        # install the exensions
     tozti = subprocess.Popen(["python", "-m", "tozti", "dev"])
     time.sleep(1)
 
