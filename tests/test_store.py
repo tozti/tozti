139ae526319d5034cb87c027bf320167d1828340
poechsel <pierre.oechsel@gmail.com>
2018-01-28 23:55:54 +0100

    tests for to-many's get

--- tests/test_store.py
+++ tests/test_store.py
@@ -248,6 +248,17 @@ def test_storage_rel_toone_get(tozti, db):
     assert result.json()["data"]["data"]["id"] == uid_bar
 
 @pytest.mark.extensions("rel01")
+def test_storage_rel_toone_get_deleted(tozti, db):
+    bar = {"attributes": {"bar": "bar"}}
+    uid_bar = add_object_get_id({"type": "rel01/bar", "attributes": {"bar": "bar"}})
+    foo = {"attributes": {"foo": "foo"}, "relationships": {"member": UUID(uid_bar)}}
+    uid_foo = add_object_get_id({"type": "rel01/foo", "attributes": {"foo": "foo"}, "relationships": {"member": {"data": {"id": uid_bar}}}})
+
+    make_call("DELETE", "/store/resources/{}".format(uid_bar))
+
+    assert make_call("GET", "/store/resources/{}/member".format(uid_foo)).status_code == 404
+
+@pytest.mark.extensions("rel01")
 def test_storage_rel_toone_get_badname(tozti, db):
     bar = {"attributes": {"bar": "bar"}}
     uid_bar = add_object_get_id({"type": "rel01/bar", "attributes": {"bar": "bar"}})
@@ -264,3 +275,19 @@ def test_storage_rel_tomany_get(tozti, db):
 
     result = make_call("GET", "/store/resources/{}/members".format(uid_foo))
     assert set(x["id"] for x in result.json()["data"]["data"]) == set([uid_bar, uid_bar2])
+
+@pytest.mark.extensions("rel02")
+def test_storage_rel_tomany_get_deleted(tozti, db):
+    uid_bar = add_object_get_id({"type": "rel02/bar", "attributes": {"bar": "bar"}})
+    uid_bar2 = add_object_get_id({"type": "rel02/bar", "attributes": {"bar": "bar"}})
+    uid_foo = add_object_get_id({"type": "rel02/foo", "attributes": {"foo": "foo"}, "relationships": {"members": {"data": [{"id": uid_bar}, {"id": uid_bar2}]}}})
+
+    make_call("DELETE", "/store/resources/{}".format(uid_bar))
+
+    assert make_call("GET", "/store/resources/{}/member".format(uid_foo)).status_code == 404
+
+@pytest.mark.extensions("rel02")
+def test_storage_rel_tomany_get_empty(tozti, db):
+    uid_foo = add_object_get_id({"type": "rel02/foo", "attributes": {"foo": "foo"}, "relationships": {"members": {"data": []}}})
+    result = make_call("GET", "/store/resources/{}/members".format(uid_foo))
+    assert len(result.json()["data"]["data"]) == 0
