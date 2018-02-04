1ee8c3b4e5cacdfce246c4a25edbd863e784ee7a
poechsel <pierre.oechsel@gmail.com>
2018-02-04 13:13:05 +0100

    Vue is now building with sass
    - added the build system
    - added the CSS parts & fonts
    - slightly adapted the build system

--- client/components/App.vue
+++ client/components/App.vue
@@ -2,7 +2,7 @@
   <div>
     <header class="main-header">
       <section class="branding">
-        <a href="#"><img src="/static/core/img/logo.svg" alt="tozti"></a>
+        <a href="#"><img :src="tozti_logo" alt="tozti"></a>
       </section>
 
       <section class="search">
@@ -35,6 +35,7 @@
 </template>
 
 <script>
+  import tozti_logo from "../assets/img/logo.svg"
   import NotificationCenter from './NotificationCenter.vue'
   import Navigation from './Navigation.vue'
 
@@ -42,6 +43,11 @@
     components: {
       NotificationCenter,
       Navigation,
+    },
+    data: function () {
+        return {
+            tozti_logo: tozti_logo
+        }
     }
   }
 </script>
