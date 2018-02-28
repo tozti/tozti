b690b5dba16dc8ce37f3da7bc2843c7c696700b4
Daniel Siladji <szilagyi.d@gmail.com>
2018-02-28 11:07:53 +0100

    Cache libsodium source and build files

--- travis/install_libsodium.sh
+++ travis/install_libsodium.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -ex
-wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
-tar xzf LATEST.tar.gz 
-cd libsodium-stable/
-./configure --prefix=/usr
-make && make check && sudo make install
+mkdir -p libsodium
+cd libsodium
+wget -O new.tar.gz https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+if [ ! -f old.tar.gz ] || [ $( cmp -s new.tar.gz old.tar.gz) ]; then
+    tar xzf new.tar.gz 
+    cd libsodium-stable/
+    ./configure --prefix=/usr
+    make && make check
+else
+    cd libsodium-stable/
+fi
+sudo make install
+cd ..
+mv new.tar.gz old.tar.gz
