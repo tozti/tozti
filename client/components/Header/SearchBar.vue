9a1dfbc5ecc4a63033a5c779dee6dcd499c36521
Romain Liautaud <romain.liautaud@ens-lyon.fr>
2018-03-02 04:12:40 +0100

    Fixed final styling.

--- client/components/Header/SearchBar.vue
+++ client/components/Header/SearchBar.vue
@@ -3,7 +3,7 @@
     <label for="search-field">
       <i class="nc-icon nc-zoom-2"></i>
     </label>
-    <input type="text" id="search-field" placeholder="Ne pas utiliser.">
+    <input type="text" id="search-field" placeholder="Rechercher des dossiers, des discussions, etc.">
   </section>
 </template>
 
--- client/components/views/FolderView.vue
+++ client/components/views/FolderView.vue
@@ -1,16 +1,22 @@
 <template>
-  <section class="section">
-    <TaxonomyFolderView :id="id"/>
-  </section>
+  <div v-if="!loading">
+    <section class="level main-title">
+      <h2 class="level-left">
+        <i class="material-icons">folder</i> {{ resource.body.name }}
+      </h2>
+    </section>
+    <section class="section">
+      <TaxonomyFolderView :id="id"/>
+    </section>
+  </div>
 </template>
 
 <script>
-  import { ModalProgrammatic } from 'buefy'
+  import { resourceMixin } from '../../mixins'
   import TaxonomyFolderView from './TaxonomyFolderView'
 
   export default {
+    mixins: [ resourceMixin ],
     components: { TaxonomyFolderView },
-
-    props: { id: String },
   }
 </script>
